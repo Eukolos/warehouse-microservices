21c3dcae7e3e1c59bfb1c2ccedeb86b6027985de
Eukolos <eminaksoy35@gmail.com>
2024-03-02 19:04:22 +0300

    added swagger to all services

---
+++ finance/src/main/java/com/istanify/finance/configuration/SpringdocConfig.java
@@ -0,0 +1,17 @@
+package com.istanify.finance.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SpringdocConfig {
+
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(new Info().title("Finance Service").version("0.0.1").description("Finance API"));
+    }
+}
