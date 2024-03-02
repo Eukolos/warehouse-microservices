b321f45c447e1271f66324e9004a6759e6157eea
Eukolos <eminaksoy35@gmail.com>
2024-03-02 17:44:55 +0300

    all services properties moved to config service

--- eureka/src/main/java/com/istanify/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/istanify/eureka/EurekaApplication.java
@@ -2,7 +2,9 @@ package com.istanify.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer // This annotation is used to enable the Eureka server
 @SpringBootApplication
 public class EurekaApplication {
 
--- finance/src/main/java/com/istanify/finance/configuration/BeanConfiguration.java
+++ finance/src/main/java/com/istanify/finance/configuration/BeanConfiguration.java
@@ -1,6 +1,7 @@
 package com.istanify.finance.configuration;
 
 import com.istanify.finance.client.AccountingClient;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -9,17 +10,29 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class BeanConfiguration {
-    public static final String ACCOUNTING_SERVICE_URL = "http://localhost:9696";
 
+    private final DiscoveryClient discoveryClient;
+
+    public BeanConfiguration(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
 
     @Bean
     AccountingClient AccountingClient() {
-        RestClient client = RestClient.create(ACCOUNTING_SERVICE_URL);
+        String accountingServiceUrl = discoveryClient.getInstances("accounting-service")
+                .stream()
+                .findFirst()
+                .map(serviceInstance -> serviceInstance.getUri().toString())
+                .orElseThrow(() -> new RuntimeException("Failed to find URL for accounting service"));
+
+        RestClient client = RestClient.create(accountingServiceUrl);
 
         HttpServiceProxyFactory factory = HttpServiceProxyFactory
                 .builderFor(RestClientAdapter.create(client))
                 .build();
         return factory.createClient(AccountingClient.class);
     }
+
+
 
 }
